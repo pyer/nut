9a079abce33c112920977d968ab61fc279ae8997
Pierre <pierre.bazonnard@gmail.com>
2013-05-11 11:05:09 +0200

    *output dir is defined in nut.xml, nothing special to do for war package

--- plugins/javaArchiver/src/java/javaArchiver.java
+++ plugins/javaArchiver/src/java/javaArchiver.java
@@ -81,11 +81,6 @@ public class javaArchiver
             throw new Exception();
         }
         
-        if ( packaging=="war" )
-        {
-           outputDirectory += File.separator + "war";
-        }
-        
         File outputDir = new File( basedir + File.separator + outputDirectory );
         if ( !outputDir.exists() )
         {
