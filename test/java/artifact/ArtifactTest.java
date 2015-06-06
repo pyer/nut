38e0dcd75c8b00b47f20505d62b1997584b925de
Pierre <pierre@bazonnard.fr>
2015-06-06 18:13:51 +0200

    + artifact full tests

--- test/java/artifact/ArtifactTest.java
+++ test/java/artifact/ArtifactTest.java
@@ -1,7 +1,10 @@
 package nut.artifact;
 
 import nut.artifact.Artifact;
+import nut.artifact.InvalidArtifactRTException;
+
 import java.io.File;
+import java.util.Properties;
 
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
@@ -28,6 +31,74 @@ public class ArtifactTest
         artifact = new Artifact( groupId, artifactId, version, type );
         assertEquals( groupId + ":" + artifactId + ":" + type + ":" + version,
                       artifact.toString() );
+    }
+
+
+    @Test(expectedExceptions = InvalidArtifactRTException.class)
+    public void testNullGroupId() throws InvalidArtifactRTException
+    {
+        new Artifact( null, artifactId, version, type );
+    }
+
+    @Test(expectedExceptions = InvalidArtifactRTException.class)
+    public void testEmptyGroupId() throws InvalidArtifactRTException
+    {
+        new Artifact( "", artifactId, version, type );
+    }
+
+    @Test(expectedExceptions = InvalidArtifactRTException.class)
+    public void testNullArtifactId() throws InvalidArtifactRTException
+    {
+        new Artifact( groupId, null, version, type );
+    }
+
+    @Test(expectedExceptions = InvalidArtifactRTException.class)
+    public void testEmptyArtifactId() throws InvalidArtifactRTException
+    {
+        new Artifact( groupId, "", version, type );
+    }
+
+    @Test(expectedExceptions = InvalidArtifactRTException.class)
+    public void testNullVersion() throws InvalidArtifactRTException
+    {
+        new Artifact( groupId, artifactId, null, type );
+    }
+
+    @Test(expectedExceptions = InvalidArtifactRTException.class)
+    public void testEmptyVersion() throws InvalidArtifactRTException
+    {
+        new Artifact( groupId, artifactId, "", type );
+    }
+
+    @Test(expectedExceptions = InvalidArtifactRTException.class)
+    public void testNullType() throws InvalidArtifactRTException
+    {
+        new Artifact( groupId, artifactId, version, null );
+    }
+
+    @Test(expectedExceptions = InvalidArtifactRTException.class)
+    public void testEmptyType() throws InvalidArtifactRTException
+    {
+        new Artifact( groupId, artifactId, version, "" );
+    }
+
+    @Test(expectedExceptions = InvalidArtifactRTException.class)
+    public void testNullRepository() throws InvalidArtifactRTException
+    {
+        String repository = System.getProperty( "nut.home" );
+        Properties sysProps = System.getProperties();
+        sysProps.remove("nut.home");
+        try {
+          new Artifact( groupId, artifactId, version, type );
+        }
+        catch ( InvalidArtifactRTException e ) {
+          System.setProperty( "nut.home", repository );
+          throw new InvalidArtifactRTException( groupId, artifactId, version, type,
+                                                  "The 'nut.home' property is undefined." );
+        }
+        finally {
+          System.setProperty( "nut.home", repository );
+        }
     }
 
 }
