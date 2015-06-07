1838fb6ef3b26bc503c8a3202366829112583a1a
Pierre <pierre@bazonnard.fr>
2015-06-07 03:42:28 +0200

    * simpler exception

--- src/java/artifact/InvalidArtifactRTException.java
+++ src/java/artifact/InvalidArtifactRTException.java
@@ -7,66 +7,15 @@ package nut.artifact;
 public class InvalidArtifactRTException
     extends RuntimeException
 {
-    
-    private final String groupId;
-    private final String artifactId;
-    private final String version;
-    private final String type;
-    private final String baseMessage;
+    private final String message;
 
-    public InvalidArtifactRTException( String groupId, String artifactId, String version, String type, String message )
+    public InvalidArtifactRTException( String message )
     {
-        this.groupId = groupId;
-        this.artifactId = artifactId;
-        this.version = version;
-        this.type = type;
-        this.baseMessage = message;
-    }
-
-    public InvalidArtifactRTException( String groupId, String artifactId, String version, String type, String message, Throwable cause )
-    {
-        super( cause );
-        
-        this.groupId = groupId;
-        this.artifactId = artifactId;
-        this.version = version;
-        this.type = type;
-        this.baseMessage = message;
+        this.message = message;
     }
 
     public String getMessage()
     {
-        return "For artifact {" + getId() + "}: " + getBaseMessage();
+        return this.message;
     }
-    
-    public String getBaseMessage()
-    {
-        return baseMessage;
-    }
-
-    public String getArtifactId()
-    {
-        return artifactId;
-    }
-
-    public String getGroupId()
-    {
-        return groupId;
-    }
-
-    public String getType()
-    {
-        return type;
-    }
-
-    public String getVersion()
-    {
-        return version;
-    }
-    
-    public String getId()
-    {
-        return groupId + ":" + artifactId + ":" + version + ":" + type;
-    }
-
 }
