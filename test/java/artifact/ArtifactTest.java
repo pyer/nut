471c3b89af40e7fde52177898681ab2a70fc81f5
Pierre <pierre.bazonnard@gmail.com>
2015-06-12 22:34:28 +0200

    + Artifact is now properly tested

--- test/java/artifact/ArtifactTest.java
+++ test/java/artifact/ArtifactTest.java
@@ -4,6 +4,9 @@ import nut.artifact.Artifact;
 import nut.artifact.InvalidArtifactRTException;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 import static org.testng.Assert.*;
@@ -11,18 +14,115 @@ import org.testng.annotations.Test;
 
 public class ArtifactTest
 {
+  /*
+  public Artifact( String groupId, String artifactId, String version, String type )
+  public String getGroupId()
+  public String getArtifactId()
+  public String getVersion()
+  public String getType()
+  public String getPath()
+  public File getFile()
+  public String mavenFilePath()
+  public String nutFilePath()
+  public boolean isPresent()
+  public FileInputStream fileInputStream()
+  public FileOutputStream fileOutputStream()
+  public String toString()
+  */
 
     private Artifact artifact;
 
     private String groupId = "groupid", artifactId = "artifactId", version = "1.0", type = "type";
 
     @Test
-    public void testPath()
+    public void testGetPath()
     {
-        //System.out.println( artifact.getPath() );
         artifact = new Artifact( groupId, artifactId, version, type );
         assertEquals( groupId + File.separator + artifactId + "-" + version + "." + type,
                       artifact.getPath() );
+    }
+
+    @Test
+    public void testGetFile()
+              throws IOException
+    {
+        String repository = System.getProperty( "nut.home" );
+        System.setProperty( "nut.home", "target" );
+        File target = new File( "target/groupid/artifactId-1.0.type" );
+        artifact = new Artifact( groupId, artifactId, version, type );
+        assertEquals( target.getCanonicalPath(), artifact.getFile().getCanonicalPath() );
+        System.setProperty( "nut.home", repository );
+    }
+
+    @Test
+    public void testMavenFilePath()
+    {
+        artifact = new Artifact( groupId, artifactId, version, type );
+        assertEquals( "/groupid/artifactId/1.0/artifactId-1.0.type", artifact.mavenFilePath() );
+    }
+
+    @Test
+    public void testNutFilePath()
+    {
+        artifact = new Artifact( groupId, artifactId, version, type );
+        assertEquals( "/groupid/artifactId-1.0.type", artifact.nutFilePath() );
+    }
+
+    @Test
+    public void testIsPresent()
+              throws IOException
+    {
+        String repository = System.getProperty( "nut.home" );
+        System.setProperty( "nut.home", "target/repo" );
+        new File( "target/repo" ).mkdir();
+        File target = new File( "target/repo/groupid/artifactId-1.0.type" );
+        new File( "target/repo/groupid" ).mkdir();
+        target.createNewFile();
+        artifact = new Artifact( groupId, artifactId, version, type );
+        assertTrue( artifact.isPresent() );
+        target.delete();
+        System.setProperty( "nut.home", repository );
+    }
+
+    @Test
+    public void testFileInputStream()
+              throws IOException
+    {
+        String repository = System.getProperty( "nut.home" );
+        System.setProperty( "nut.home", "target/repo" );
+        new File( "target/repo" ).mkdir();
+        File target = new File( "target/repo/groupid/artifactId-1.0.type" );
+        new File( "target/repo/groupid" ).mkdir();
+        target.createNewFile();
+        FileOutputStream ft = new FileOutputStream(target);
+        ft.write( 33 );
+        ft.close();
+        artifact = new Artifact( groupId, artifactId, version, type );
+        FileInputStream f = artifact.fileInputStream();
+        int c = f.read();
+        f.close();
+        target.delete();
+        System.setProperty( "nut.home", repository );
+        assertEquals( c, 33 );
+    }
+
+    @Test
+    public void testFileOutputStream()
+              throws IOException
+    {
+        System.setProperty( "nut.home", "target/repo" );
+        new File( "target/repo" ).mkdir();
+        File target = new File( "target/repo/groupid/artifactId-1.0.type" );
+        new File( "target/repo/groupid" ).mkdir();
+        String repository = System.getProperty( "nut.home" );
+        target.createNewFile();
+        artifact = new Artifact( groupId, artifactId, version, type );
+        FileOutputStream f = artifact.fileOutputStream();
+        // Test is OK when write is OK, no exception
+        f.write( 33 );
+        f.close();
+        target.delete();
+        System.setProperty( "nut.home", repository );
     }
 
     @Test
