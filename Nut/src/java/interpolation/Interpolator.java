d917189282e50b81ca6ac334b419ef9b6461ba59
Pierre <pierre.bazonnard@gmail.com>
2016-05-31 13:36:12 +0200

    + comments

--- Nut/src/java/interpolation/Interpolator.java
+++ Nut/src/java/interpolation/Interpolator.java
@@ -14,17 +14,19 @@ import nut.logging.Log;
 /**
  * Interpolator replaces every data as "${xx} by the value of the xx property.
  * For example: <version>${project.version}</version> is replaced by the project version in the project model
+ *
+ * Does nothing at the moment.
+ * will be done later, when I'll need this feature
  */
 public class Interpolator
 {
-    
     private Model model;
 
     private Artifact artifact;
 
     // Building time
     private long time;
-    // 
+    //
     boolean buildDone;
     boolean buildSuccess;
 
@@ -42,6 +44,4 @@ public class Interpolator
     {
       return model;
     }
-
 }
-
