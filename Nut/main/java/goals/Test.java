2d050096cbcca463d9dfa35c353232d2063df5aa
Pyer <pierre.bazonnard@gmail.com>
2017-04-26 16:54:50 +0200

    - remove old comment

--- Nut/main/java/goals/Test.java
+++ Nut/main/java/goals/Test.java
@@ -39,7 +39,6 @@ public class Test
             String command   = System.getProperty( "java.home", "/usr" ) + "/bin/java";
             String classpath = testOutputDirectory + ":" + outputDirectory + project.getDependenciesClassPath();
             log.debug("classpath = " + classpath);
-            //"java -Xmx512M -classpath $NUT_HOME/nut/Nut-$NUT_VERSION.jar:$NUT_HOME/org/codehaus/plexus/plexus-utils-3.0.jar:$NUT_HOME/com/beust/jcommander-1.7.jar:$NUT_HOME/org/testng/testng-6.8.7.jar:/home/pba/nutRepository/javax/servlet/javax.servlet-api-3.1.0.jar:/home/pba/nutRepository/org/slf4j/slf4j-api-1.7.18.jar:/home/pba/nutRepository/org/quartz-scheduler/quartz-2.2.2.jar nut.Nut "-Dversion=$NUT_VERSION" "-Dhome=$NUT_HOME" $*";
             // Run a java app in a separate system process
             ProcessBuilder pb = new ProcessBuilder(command, "-cp", classpath, "-Dbasedir=" + basedir,
                                                   "nut.TestRunner", testSuiteFileName, testReportDirectory);
