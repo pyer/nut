78c4133f57f0b97c44277ddffa42caf016bea67f
pyer <pierre.bazonnard@gmail.com>
2020-12-23 18:08:23 +0100

    + copy resource to output when pattern is jar

--- Nut/src/main/java/goals/Pack.java
+++ Nut/src/main/java/goals/Pack.java
@@ -9,6 +9,12 @@ import nut.model.Project;
 import java.io.File;
 import java.io.IOException;
 
+import java.nio.file.CopyOption;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,6 +55,17 @@ public class Pack implements Goal
           if ( !outputDir.exists() ) {
             throw new GoalException("\'" + outputDirectory + "\' is empty");
           }
+
+          File resourceDir = new File( basedir + File.separator + resourceDirectory );
+          if ( resourceDir.exists() ) {
+            try {
+            // Copy resource to output
+            copyResource(resourceDir, outputDir);
+            } catch ( IOException e) {
+              throw new GoalException(e.getMessage());
+            }
+          }
+
           Jar jar = new Jar(fullName);
           jar.archive(basedir + File.separator + outputDirectory);
         } else if ( "zip".equals(packaging) ) {
@@ -56,4 +73,42 @@ public class Pack implements Goal
           zip.archive(basedir + File.separator + resourceDirectory);
         }
     }
+
+    // ==========================================================================
+    private void copyResource(File source, File dest) throws IOException {
+        log.debug("* copy " + source.getPath() + " to " + dest.getPath());
+        CopyOption options = StandardCopyOption.COPY_ATTRIBUTES;
+        if (source.isDirectory())
+            copyFolder(source, dest, options);
+        else {
+            ensureParentFolder(dest);
+            copyFile(source, dest, options);
+        }
+    }
+
+    private void copyFolder(File source, File dest, CopyOption... options) throws IOException {
+        if (!dest.exists())
+            dest.mkdirs();
+        File[] contents = source.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                File newFile = new File(dest.getCanonicalPath() + File.separator + f.getName());
+                if (f.isDirectory())
+                    copyFolder(f, newFile, options);
+                else
+                    copyFile(f, newFile, options);
+            }
+        }
+    }
+
+    private void copyFile(File source, File dest, CopyOption... options) throws IOException {
+        Files.copy(source.toPath(), dest.toPath(), options);
+    }
+
+    private void ensureParentFolder(File file) {
+        File parent = file.getParentFile();
+        if (parent != null && !parent.exists())
+            parent.mkdirs();
+    }
+
 }
