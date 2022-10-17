64bf76f44c6363a0b38fbaf2e82af2e901fa8633
pyer <pierre.bazonnard@gmail.com>
2022-10-17 11:11:18 +0200

    + Hello displays optional args

--- samples/hello/main/java/Hello.java
+++ samples/hello/main/java/Hello.java
@@ -3,7 +3,11 @@ public class Hello
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello world !" );
+        String world = "world";
+        if ( args.length > 0 ) {
+          world = args[0];
+        }
+        System.out.println( "Hello " + world );
         System.exit( 0 );
     }
 }
